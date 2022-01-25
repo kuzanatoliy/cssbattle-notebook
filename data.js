550269e6d353562b591d8b65dcd6db16cb489a00
kuzanatoliy <njkznu@gmail.com>
2022-01-25 22:27:28 +0300

    add push button

--- data.js
+++ data.js
@@ -6,8 +6,12 @@ module.exports = [{
     name: "Simply Square",
     solution: require('./01-simply-square'),
   }, {
-    id: 1,
+    id: 2,
     name: "Carrom",
     solution: require('./02-carrom'),
+  }, {
+    id: 3,
+    name: "Push Button",
+    solution: require('./03-push-button'),
   }],
 }]
