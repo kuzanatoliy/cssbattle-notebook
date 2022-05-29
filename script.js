3b4ebeb99d6016bc653be27d65597a541d221acf
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2022-05-29 22:51:29 +0300

    chore: add deploy job

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const { writeFile, mkdir } = require("fs/promises");
 const files = data[0].plays;
 
 const runDeploy = async () => {
-  await mkdir("1_pilot_battle");
+  await mkdir("dist");
+  await mkdir("dist/1_pilot_battle");
 
   await Promise.all(
     files.map((item) =>
       writeFile(
-        `1_pilot_battle/${item.id}_${item.name
+        `dist/1_pilot_battle/${item.id}_${item.name
           .toLocaleLowerCase()
           .replaceAll(" ", "_")}.html`,
         createPlay(item)
