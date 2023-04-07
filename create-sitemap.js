0d71a984065a57638ca0db852a794bdb1cb5ad4d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-04-07 07:04:27 +0300

    fix: sitemap dates

--- create-sitemap.js
+++ create-sitemap.js
@@ -3,21 +3,37 @@ const date = new Date();
 module.exports = ({ list, root }) => `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${ root ? `
+  ${
+    root
+      ? `
     <url>
-      <loc>https://kuzanatoliy.github.io/cssbattle-notebook/${root.mapPath}</loc>
-      <lastmod>${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}</lastmod>
+      <loc>https://kuzanatoliy.github.io/cssbattle-notebook/${
+        root.mapPath
+      }</loc>
+      <lastmod>${date.getUTCFullYear()}-${date
+          .getUTCMonth()
+          .toString()
+          .padStart(2, "0")}-${date
+          .getUTCDate()
+          .toString()
+          .padStart(2, "0")}</lastmod>
       <changefreq>monthly</changefreq> 
       <priority>1</priority>
     </url>
-  ` : {} }
-  ${ list.map((item) => `
+  `
+      : {}
+  }
+  ${list
+    .map(
+      (item) => `
     <url>
       <loc>https://kuzanatoliy.github.io/cssbattle-notebook/${item.mapPath}</loc>
       <lastmod>${item.date}</lastmod>
       <changefreq>never</changefreq> 
       <priority>1</priority>
     </url>
-  `).join('') }
+  `
+    )
+    .join("")}
 </urlset>
 `;
