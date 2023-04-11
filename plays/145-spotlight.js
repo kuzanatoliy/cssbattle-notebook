184414f75850bceb389a565f1354cd66f700b06f
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-04-11 12:26:23 +0300

    feat: add spotlight play

---
+++ plays/145-spotlight.js
@@ -0,0 +1,21 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #E3516E;
+  }
+  body, p {
+    background: #D9D9D9;
+    margin: 0 0 150px 250px;
+  }
+  p {
+    width: 150px;
+    height: 150px;
+    left: -50px;
+    top: 50px;
+    position: fixed;
+    border-radius: 0 0 50% 50%;
+    transform: rotate(45deg);
+  }
+</style>
+`;
