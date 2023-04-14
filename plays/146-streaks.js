c4142ede6f59175ffbfe9c673dcf2e2e61bcdfa3
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-04-14 17:11:25 +0300

    feat: add streaks play

---
+++ plays/146-streaks.js
@@ -0,0 +1,14 @@
+module.exports = `
+<style>
+  * {
+    background: #998235;
+  }
+  body {
+    transform: rotate(315deg);
+    position: relative;
+    left: 169px;
+    top: 169px;
+    background: repeating-linear-gradient(#FCBE5C, #FCBE5C 25px, #0B2429 25px, #0B2429 50px, #998235 50px, #998235 75px);
+  }
+</style>
+`;
