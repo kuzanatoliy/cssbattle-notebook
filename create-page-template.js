ed141aefd5993cc3f57e6b608c4b3d58804d5022
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-06-17 12:28:31 +0300

    feat: redesign root page

---
+++ create-page-template.js
@@ -0,0 +1,108 @@
+module.exports = ({
+  title,
+  description,
+  keyworkds,
+  canonicalPath,
+  injectStyles,
+  injectScripts,
+  injectBody,
+}) => `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>${title}</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="${description}">
+    <meta name="keywords" content="${keyworkds}">
+    <meta name="author" content="Anatoli Kuzmiankou">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    ${
+      canonicalPath
+        ? `<link rel="canonical" href="https://kuzanatoliy.github.io${canonicalPath}"/>`
+        : ""
+    }
+    <style>
+
+:root {
+  --color-dark: #02315E;
+  --color-normal: #00457E;
+  --color-light: #2F70AF;
+  --color-second: #B9848C;
+  --color-third: #806491;
+  --color-white: #fff;
+
+  --size-quarter: 0.125rem;
+  --size-half: 0.25rem;
+  --size-three-quarters: 0.375rem;
+  --size-single: 0.5rem;
+  --size-double: 1rem;
+  --size-triple: 1.5rem;
+  --size-quadruple: 2rem;
+
+  --breakpoint-laptop: 1024px;
+  --breakpoint-tablet: 768px;
+  --breakpoint-mobile-l: 425px;
+  --breakpoint-mobile-m: 375px;
+  --breakpoint-mobile-s: 320px;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  margin: 0;
+  font-size: 16px;
+}
+
+header, main, footer {
+  margin: 0;
+  padding: var(--size-double)
+    calc((100% - var(--breakpoint-laptop)) / 2 + var(--size-double));
+}
+
+@media (max-width: 1056px) {
+  header, main, footer {
+    padding: var(--size-double);
+  }
+}
+
+main {
+  margin-top: var(--size-double);
+  flex: 1;
+}
+
+header {
+  color: var(--color-white);
+  position: sticky;
+  top: 0;
+  background: var(--color-dark);
+  box-shadow: 0 var(--size-double) var(--size-double) var(--color-white);
+}
+
+footer {
+  text-align: center;
+  position: sticky;
+  bottom: 0;
+  background: linear-gradient(transparent, var(--color-white) 25%);
+}
+
+header > h1 {
+  margin: 0;
+}
+
+${injectStyles ? injectStyles() : ""}
+    
+    </style>
+  </head>
+  <body>
+    <header><h1>CSS battle solutions</h1></header>
+    <main>
+
+${injectBody ? injectBody() : ""}
+
+    </main>
+    ${injectScripts ? `<script>${injectScripts()}</script>` : ""}
+    <footer class={styles.footerContainer}>© Kuzanatoliorg 2023</footer>
+  </body>
+</html>
+`;
--- create-root.js
+++ create-root.js
@@ -1,27 +1,14 @@
-module.exports = ({ list, canonicalPath }) => `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>CSS battle: solutions</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="Solution of CSS battle">
-    <meta name="keywords" content="html, css, cssbattle, coding, development, engineering, inclusive, community, programming">
-    <meta name="author" content="Anatoli Kuzmiankou">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    ${
-      canonicalPath
-        ? `<link rel="canonical" href="https://kuzanatoliy.github.io${canonicalPath}"/>`
-        : ""
-    }
-  </head>
-  <body>
-    <header><h1>CSS battle solutions</h1></header>
-    <hr>
-    <main>
-    ${list
-      .map((item) => `<a href=${item.path}>${item.id} - ${item.name}</a>`)
-      .join("<br/>")}
-    </main>
-  </body>
-</html>
-`;
+const createPageTemplate = require("./create-page-template");
+
+module.exports = ({ list, canonicalPath }) =>
+  createPageTemplate({
+    title: "CSS battle: solutions",
+    description: "Solution of CSS battle",
+    keyworkds:
+      "html, css, cssbattle, coding, development, engineering, inclusive, community, programming",
+    canonicalPath,
+    injectBody: () =>
+      list
+        .map((item) => `<a href=${item.path}>${item.id} - ${item.name}</a>`)
+        .join("<br/>"),
+  });
