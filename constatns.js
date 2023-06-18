059c601e52677c4f05062f207a00f3162eb3b31b
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-06-18 18:24:50 +0300

    feat: root page redesign

---
+++ constatns.js
@@ -0,0 +1,4 @@
+module.exports = {
+  HOST_NAME: "https://kuzanatoliy.github.io",
+  ROOT_PATH: "cssbattle-notebook",
+};
--- create-root.js
+++ create-root.js
@@ -1,4 +1,5 @@
 const createPageTemplate = require("./create-page-template");
+const { HOST_NAME, ROOT_PATH } = require("./constatns");
 
 module.exports = ({ list, canonicalPath }) =>
   createPageTemplate({
@@ -6,17 +7,67 @@ module.exports = ({ list, canonicalPath }) =>
     description: "Solution of CSS battle",
     keyworkds:
       "html, css, cssbattle, coding, development, engineering, inclusive, community, programming",
-    canonicalPath,
+    canonicalPath: `${HOST_NAME}/${ROOT_PATH}/${canonicalPath}`,
     injectBody: () =>
       `<ul>${list
         .map(
           (item) =>
-            `<li><a href=${item.path}>${item.id} - ${item.name}</a></li>`
+            `<li><a href=${item.path}><h2>${item.id} - ${
+              item.name
+            }</h2><div>${item.playsList
+              .map(
+                (play) =>
+                  `<img
+                  alt="#${play.id} - ${play.name} preview"
+                  src="https://cssbattle.dev/targets/${play.id}.png"
+                  srcset="https://cssbattle.dev/targets/${play.id}@2x.png 2x"
+                />`
+              )
+              .join("")}</div></a></li>`
         )
-        .join("<br/>")}<ul>`,
+        .join("")}<ul>`,
     injectStyles: () => `
       ul {
         list-style-type: none;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+        grid-gap: var(--size-triple);
+        margin: 0;
+        padding: 0;
+      }
+
+      a {
+        display: block;
+        padding: var(--size-double);
+        border: solid 1px var(--color-light);
+        border-radius: var(--size-three-quarters);
+        text-decoration: none;
+        color: var(--color-dark);
+        height: 100%;
+        box-sizing: border-box;
+      }
+
+      a:active, a:focus-visible {
+        border: none;
+        outline: solid 2px var(--color-dark);
+      }
+
+      a:hover {
+        backdrop-filter: brightness(0.95);
+      }
+
+      a > h2 {
+        margin: 0;
+      }
+
+      a > div {
+        display: flex;
+        flex-wrap: wrap;
+        margin-top: var(--size-double);
+      }
+
+      a > div > img {
+        width: calc(100% / 3);
       }
     `,
   });
