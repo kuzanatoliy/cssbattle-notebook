8214f97e933211d40c2cd7a7cf9dd3a5b8c5185a
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-07-02 00:05:59 +0300

    feat: add 2023 06 29 target

---
+++ daily-targets/2023-06-29.js
@@ -0,0 +1,31 @@
+// no score
+module.exports = `
+<p><p a><p b>
+<style>
+  * {
+    background: linear-gradient(90deg, #F5D6B4 185px, 0, #D86F45 215px, 0, #F5D6B4);
+    border-radius: 99q;
+  }
+  body {
+    margin: 60px 130px;
+    background: #D86F45;
+  }
+  p {
+    position: fixed;
+    width: 50px;
+    height: 50px;
+    top: 109px;
+    left: 105px;
+  }
+  [a] {
+    left: 175px;
+  }
+  [b] {
+    left: 245px;
+  }
+</style>
+`;
+
+/*
+<p><p a><p b><style>*{background:linear-gradient(90deg,#F5D6B4 185px,0,#D86F45 215px,0,#F5D6B4);border-radius:99q}body{margin:60 130;background:#D86F45}p{position:fixed;width:50;height:50;top:109;left:105}[a]{left:175}[b]{left:245
+ */
