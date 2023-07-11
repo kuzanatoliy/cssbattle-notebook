3a2dc1b40b71eee80cf359ac117c37255b11193f
kuzanatoliy <njkznu@gmail.com>
2023-07-11 07:27:11 +0300

    feat: add 2023 07 11 target

---
+++ daily-targets/2023-07-11.js
@@ -0,0 +1,18 @@
+module.exports = `
+<p><p>
+<style>
+  * {
+    background: #926927;
+  }
+  p {
+    width: 0;
+    border-bottom: 135px solid #F8B140;
+    border-left: 90px solid transparent;
+    margin: 40px 0 0 112px;
+  }
+  p + p {
+    scale: -1 -1;
+    margin: -50px 0 0 182px;
+  }
+</style>
+`;
