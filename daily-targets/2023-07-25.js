01dbd9d56eb3f28f5b2744fc0a93c9bc8d6fbe34
kuzanatoliy <njkznu@gmail.com>
2023-07-25 07:54:27 +0300

    feat: add 2023 07 25 target

---
+++ daily-targets/2023-07-25.js
@@ -0,0 +1,30 @@
+module.exports = `
+<dl><p></dl><dl><p>
+<style>
+  * {
+    background: #926927;
+    float: left;
+    border-radius: 9in;
+    margin: 0;
+  }
+  dl {
+    margin: 25px 0 0 115px;
+    width: 30px;
+    height: 250px;
+    background: #F8B140;
+  }
+  dl + dl {
+    margin-left: 110px;
+    scale: -1;
+  }
+  p {
+    position: relative;
+    top: 45px;
+    left: -35px;
+    width: 100px;
+    height: 100px;
+    background: #F8B140;
+    box-shadow: 0 0 0 10px #926927;
+  }
+</style>
+`;
