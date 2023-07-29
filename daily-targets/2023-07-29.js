0fdfd017db64b5444713c905ac4bc1f503a45be3
kuzanatoliy <njkznu@gmail.com>
2023-07-29 12:05:38 +0300

    feat: add 2023 07 29 target

---
+++ daily-targets/2023-07-29.js
@@ -0,0 +1,14 @@
+module.exports = `
+<style>
+  * {
+    background: #E3516E;
+    color: #FADE8B;
+  }
+  * * {
+    background: #FADE8B;
+    rotate: 45deg;
+    margin: 70px 120px;
+    box-shadow: 190px 0, -190px 0, 0 190px, 0 -190px;
+  }
+</style>
+`;
