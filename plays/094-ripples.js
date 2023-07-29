cc4cd5953a7145830c7993f66bd78ca913238a7d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-07-29 14:04:08 +0300

    feat: add ripples play

---
+++ plays/094-ripples.js
@@ -0,0 +1,10 @@
+module.exports = `
+<style>
+  * {
+    background: #0E2E2C;
+  }
+  body {
+    margin: 50px 100px;
+    background: radial-gradient(#F3AC3C 20px,0,#0E2E2C 28px,0,#F3AC3C 40px,0,#0E2E2C 50px,0,#998235 60px,0,#0E2E2C 72px,0,#F3AC3C 80px,0,#0E2E2C 94px,0,#F3AC3C 100px,0,#0E2E2C);  
+</style>
+`;
