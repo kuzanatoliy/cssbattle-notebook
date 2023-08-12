f365e1801975f61c3290bcaf1fb3042f11986e45
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-08-12 23:49:15 +0300

    feat: add 2023 08 12 target

---
+++ daily-targets/2023-08-12.js
@@ -0,0 +1,14 @@
+module.exports = `
+<style>
+  * {
+    background: #E3516E;
+    color: #FADE8B;
+  }
+  * * {
+    margin: 0 -40px;
+    width: 100px;
+    height: 150px;
+    box-shadow: 115px -50px, 115px 150px, 265px 0, 265px 200px;
+  }
+</style>
+`;
