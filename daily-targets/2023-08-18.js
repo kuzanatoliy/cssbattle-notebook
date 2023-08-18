71e5fba9fe792a2a86b22790f805ddcd725c93ec
kuzanatoliy <njkznu@gmail.com>
2023-08-18 21:58:19 +0300

    feat: add 2023 08 18 target

---
+++ daily-targets/2023-08-18.js
@@ -0,0 +1,12 @@
+module.exports = `
+<style>
+  * {
+    background: #62306D;
+  }
+  * * {
+    background: #F7EC7D;
+    margin: 50px 110px;
+    clip-path: polygon(128px 0, 0 100%, 100% 100%);
+  }
+</style>
+`;
