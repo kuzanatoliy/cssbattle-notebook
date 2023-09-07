880e3056b79a85aadfc2d56331369eaf8801d3ee
kuzanatoliy <njkznu@gmail.com>
2023-09-07 07:59:13 +0300

    feat: add 2023 09 07 target

---
+++ daily-targets/2023-09-07.js
@@ -0,0 +1,23 @@
+module.exports = `
+<p><p>
+<style>
+  * {
+    background: #C52184;
+    border-radius: 9in;
+  }
+  * * {
+    background: #F9B4ED;
+    margin: 100px 50px;
+  }
+  p {
+    float: left;
+    margin: 0;
+    height: 100px;
+    width: 100px;
+    outline: solid 25px #C52184;
+  }
+  p + p {
+    margin-left: 100px;
+  }
+</style>
+`;
