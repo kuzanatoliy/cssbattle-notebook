a92eea5b4c74d827d40c244080dd925fa6ee414a
kuzanatoliy <njkznu@gmail.com>
2023-09-16 11:03:23 +0300

    feat: add pyramid play

---
+++ plays/165-pyramid.js
@@ -0,0 +1,23 @@
+module.exports = `
+<p><p>
+<style>
+  * {
+    background: #F0CD48;
+    color: #66284A;
+  }
+  body {
+    margin: 0 300px 200px 0;
+    border-radius: 9in;
+    box-shadow: 150px 30px, 50px 170px, 250px 170px;
+  }
+  p {
+    position: fixed;
+    inset: 64px 100px;
+    clip-path: polygon(50% 0, 100% 100%, 0 100%);
+  }
+  p + p {
+    background: #66284A;
+    inset: 89px 130px 79px;
+  }
+</style>
+`;
