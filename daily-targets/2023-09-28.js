7d67175c30be20b5d441b20bf218410650cffe53
kuzanatoliy <njkznu@gmail.com>
2023-09-28 07:38:40 +0300

    feat: add 2023 09 28 targets

---
+++ daily-targets/2023-09-28.js
@@ -0,0 +1,14 @@
+module.exports = `
+<style>
+  * {
+    background: #5E2D8C;
+  }
+  * * {
+    background: #F7ED5A;
+    margin: 130px 220px 130px 140px;
+    color: #F7ED5A;
+    box-shadow: 0 0 0 40px #5E2D8C, -40px -40px 0 40px, -40px 40px 0 40px, 40px 80px;
+    -webkit-box-reflect: right 40px;
+  }
+</style>
+`;
