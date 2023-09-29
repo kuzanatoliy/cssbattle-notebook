18f45a923ba8131a16cc83b133865bcfa735d452
kuzanatoliy <njkznu@gmail.com>
2023-09-29 07:40:03 +0300

    feat: add 2023 09 29 targets

---
+++ daily-targets/2023-09-29.js
@@ -0,0 +1,22 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #E69041;
+  }
+  * * {
+    margin: 110px 80px 70px;
+    border: solid 40px #221F20;
+    border-radius: 9in;
+  }
+  body {
+    border-top: none;
+    border-radius: 0 0 9in 9in;
+  }
+  p {
+    position: fixed;
+    top: -40px;
+    left: 80px;
+  }
+</style>
+`;
