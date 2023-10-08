07272cbddd7b9e7f85781109b57b55edbc98e6f6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-10-08 12:26:27 +0300

    feat: add 2023 10 08 target

---
+++ daily-targets/2023-10-08.js
@@ -0,0 +1,16 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: radial-gradient(circle, #ED6A9D 80px, 0, #050044 120px, 0, #ED6A9D);
+  }
+  * * {
+    background: #ED6A9D;
+    margin: 130px 0;
+  }
+  p {
+    height: 100%;
+    rotate: 90deg;
+  }
+</style>
+`;
