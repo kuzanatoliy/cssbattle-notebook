1a1c4c3b05332b29721382895e8545250f4e97c0
kuzanatoliy <njkznu@gmail.com>
2023-10-21 22:23:22 +0300

    feat: add 2023 10 21 target

---
+++ daily-targets/2023-10-21.js
@@ -0,0 +1,30 @@
+module.exports = `
+<p><div><p>
+<style>
+  * {
+    background: #4F77FF;
+  }
+  * * {
+    margin: 0;
+  }
+  p {
+    height: 100vw;
+    width: 100vw;
+    border-radius: 9in;
+    background: #284ED0;
+    margin-top: 150px;
+  }
+  div {
+    background: #FFF;
+    position: fixed;
+    height: 100px;
+    width: 100px;
+    inset: 100px 150px;
+    overflow: hidden;
+  }
+  div > p {
+    margin: 50px -150px;
+    background: #8092D0;
+  }
+</style>
+`;
