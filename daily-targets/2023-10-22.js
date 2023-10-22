c099374a2f71be4a903925c90be8e99779b43888
kuzanatoliy <njkznu@gmail.com>
2023-10-22 13:11:27 +0300

    feat: add 2023 10 22 target

---
+++ daily-targets/2023-10-22.js
@@ -0,0 +1,22 @@
+module.exports = `
+<p><p a><p>
+<style>
+  * {
+    background: #5E2D8C;
+    margin: 0;
+  }
+  body {
+    background: #42A5F5;
+    margin: 60px 70px;
+    padding: 30px;
+  }
+  p {
+    height: 30px;
+  }
+  [a] {
+    width: 30px;
+    height: 60px;
+    margin-left: 85px;
+  }
+</style>
+`;
