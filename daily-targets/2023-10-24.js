cd3d645890f8333def81c94350ac927db4a7e6c5
kuzanatoliy <njkznu@gmail.com>
2023-10-24 07:30:54 +0300

    feat: add 2023 10 24 target

---
+++ daily-targets/2023-10-24.js
@@ -0,0 +1,12 @@
+module.exports = `
+<style>
+  * {
+    background: linear-gradient(90deg, #E3B52E 125px, 0, #8C292A 155px, 0, #E3B52E 185px, 0, #8C292A 215px, 0, #E3B52E 245px, 0, #8C292A 275px, 0, #E3B52E);
+  }
+  * * {
+    border: solid 30px #8C292A;
+    margin: 75px 125px;
+    rotate: 45deg;
+  }
+</style>
+`;
