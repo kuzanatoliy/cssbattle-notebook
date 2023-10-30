a801d7b1301ebbf768547abeefdb5aae6dac5708
kuzanatoliy <njkznu@gmail.com>
2023-10-30 07:29:52 +0300

    fix: 2023 10 29 target

--- daily-targets/2023-10-28.js
+++ daily-targets/2023-10-28.js
@@ -1,29 +1,21 @@
 module.exports = `
-<dl><dl><dl><dl>
+<p>
 <style>
   * {
-    background: #872626;
-    border-radius: 9in;
+    background: #B8B7CC;
   }
   * * {
-    background: #E3745D;
+    background: #1E1E5A;
+    margin: 75px;
   }
-  body {
-    margin: 125px 175px;
-    outline: solid #E3745D 20px;
-    outline-offset: 40px;
-  }
-  dl {
-    position: absolute;
-    height: 60px;
-    width: 20px;
-    inset: 45px 190px;
-    rotate: 90deg;
-    transform-origin: 10px 105px;
-    margin: 0;
-  }
-  dl > dl {
-    inset: 0;
+  p {
+    position: fixed;
+    height: 100px;
+    width: 100px;
+    background: linear-gradient(#1E1E5A 50%, 0, #B8B7CC);
+    border-radius: 9in;
+    top: -50px;
+    -webkit-box-reflect: below 50px;
   }
 </style>
 `;
