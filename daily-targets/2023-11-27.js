3b35b4a27282d041f195f22bd96c0d9d935fabc2
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-11-27 07:28:34 +0300

    feat: add 2023 11 27 target

---
+++ daily-targets/2023-11-27.js
@@ -0,0 +1,20 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #DCD68D;
+  }
+  * * {
+    border: solid 30px #524354;
+    border-radius: 0 0 9in 9in;
+    margin: 105px 65px 45px;
+  }
+  p {
+    position: fixed;
+    scale: -1;
+    margin: -90px 30px;
+    width: 90px;
+    height: 30px;
+  }
+</style>
+`;
