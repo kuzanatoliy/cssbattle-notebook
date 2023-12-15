869e447e6a09edf24b01424b3ae4a5ddc6d92dd2
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-15 07:36:23 +0300

    feat: add 2023 12 15 target

---
+++ daily-targets/2023-12-15.js
@@ -0,0 +1,20 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #FFF45F;
+  }
+  * * {
+    background: linear-gradient(90deg, #403571 50%, 0, #0098D1);
+    margin: 30px 80px;
+    padding: 60px;
+  }
+  body {
+    clip-path: polygon(25% 0, 50% 25%, 75% 0, 75% 25%, 100% 25%, 75% 50%, 100% 75%, 75% 75%, 75% 100%, 50% 75%, 25% 100%, 25% 75%, 0 75%, 25% 50%, 0 25%, 25% 25%);
+  }
+  p {
+    margin: 0;
+    scale: -1;
+  }
+</style>
+`;
