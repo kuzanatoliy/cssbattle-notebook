5b6cde69ab19e492278ff794502d6c197d8188c5
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-02-24 22:10:12 +0300

    feat: add modern fence play

---
+++ plays/191-modern-fence.js
@@ -0,0 +1,35 @@
+module.exports = `
+<p><p a><p c><p c b><p c b d>
+<style>
+  * {
+    background: #2F2E59;
+  }
+  p {
+    background: #EEECF6;
+    position: fixed;
+    height: 150px;
+    width: 20px;
+    border-radius: 9in;
+    left: 77px;
+    top: 59px;
+  }
+  [a] {
+    left: 303px;
+  }
+  [c] {
+    height: 20px;
+    width: 273px;
+    top: 124px;
+    left: 63px;
+    rotate: 30deg;
+  }
+  [b] {
+    top: 182px;
+    width: 246px;
+  }
+  [d] {
+    top: 66px;
+    left: 91px;
+  }
+</style>
+`;
