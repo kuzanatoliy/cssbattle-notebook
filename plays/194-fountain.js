33dcef4d30e5223346867ac5c116afadfac04c22
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-03-05 23:06:40 +0300

    feat: add fountain play

---
+++ plays/194-fountain.js
@@ -0,0 +1,28 @@
+module.exports = `
+<p><p><dl>
+<style>
+  * {
+    background: #4FA07B;
+    margin: 0;
+    position: fixed;
+    height: 80px;
+    width: 20px;
+  }
+  p {
+    border-radius: 9in 9in 0 0;
+    border: double 60px #0D1335;
+    border-bottom: none;
+    margin: 20px 130px;
+  }
+  p + p {
+    margin: 120px 70px;
+    height: 57px;
+    -webkit-box-reflect: right -20px;
+  }
+  dl {
+    height: 65px;
+    color: #0D1335;
+    box-shadow: 190px 59vw, 50vh 215px, 110px 65vh, 230px 215px, 90vh 65vh;
+  }
+</style>
+`;
