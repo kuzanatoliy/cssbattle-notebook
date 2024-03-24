ffba37dd93acab8b98c717b59640fd469a05e164
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-03-24 14:34:28 +0300

    feat: add negative box play

---
+++ plays/114-negative-box.js
@@ -0,0 +1,12 @@
+module.exports = `
+<style>
+  * {
+    background: #172D2C;
+    * {
+      background: #E9AF53;
+      margin: 20px;
+      clip-path: polygon(0 25%, 50% 50%, 100% 25%, 50% 0, 50% 100%, 0 75%);
+    }
+  }
+</style>
+`;
