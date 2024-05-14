d061bd3cbc0a2862dbd28211db2facd65c28a24c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-05-14 09:31:06 +0300

    feat: add 14 05 2024 target

---
+++ daily-targets/2024-05-14.js
@@ -0,0 +1,20 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #611B3F;
+    * {
+      background: #BE3184;
+      margin: 50px 100px;
+      scale: -1;
+    }
+    p {
+      border: solid 40px;
+      margin: 0;
+      border-bottom: none;
+      border-color: #A1226D #611B3F;
+      height: 80px;
+    }
+  }
+</style>
+`;
