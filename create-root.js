a35b647f4f4c49db90f66257833ffc34ce03edc8
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-07-01 13:17:30 +0300

    feat: CN-34 move new icons to assets directory fix styles

--- create-root.js
+++ create-root.js
@@ -27,49 +27,51 @@ module.exports = ({ list, canonicalPath }) =>
         )
         .join("")}</ul>`,
     injectStyles: () => `
-      ul {
-        list-style-type: none;
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
-        grid-gap: var(--size-triple);
-        margin: 0;
-        padding: 0;
-      }
+      main {
+        ul {
+          list-style-type: none;
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+          grid-gap: var(--size-triple);
+          margin: 0;
+          padding: 0;
+        }
 
-      a {
-        display: block;
-        padding: var(--size-double);
-        border: solid 1px var(--control-border-color);
-        border-radius: var(--size-three-quarters);
-        text-decoration: none;
-        color: var(--control-text-color);
-        height: 100%;
-        box-sizing: border-box;
-      }
+        a {
+          display: block;
+          padding: var(--size-double);
+          border: solid 1px var(--control-border-color);
+          border-radius: var(--size-three-quarters);
+          text-decoration: none;
+          color: var(--control-text-color);
+          height: 100%;
+          box-sizing: border-box;
+        }
 
-      a:active, a:focus-visible {
-        border: none;
-        outline: solid 2px var(--control-active-border-color);
-      }
+        a:active, a:focus-visible {
+          border: none;
+          outline: solid 2px var(--control-active-border-color);
+        }
 
-      a:hover {
-        backdrop-filter: brightness(0.95);
-      }
+        a:hover {
+          backdrop-filter: brightness(0.95);
+        }
 
-      a > h2 {
-        margin: 0;
-      }
+        a > h2 {
+          margin: 0;
+        }
 
-      a > div {
-        display: flex;
-        flex-wrap: wrap;
-        margin-top: var(--size-double);
-      }
+        a > div {
+          display: flex;
+          flex-wrap: wrap;
+          margin-top: var(--size-double);
+        }
 
-      a > div > battle-preview {
-        width: calc(100% / 3);
-        height: auto;
-        aspect-ratio: 4 / 3;
+        a > div > battle-preview {
+          width: calc(100% / 3);
+          height: auto;
+          aspect-ratio: 4 / 3;
+        }
       }
     `,
   });
