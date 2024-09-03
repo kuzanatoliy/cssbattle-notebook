95b6cd600201f12183fe5db73debdb0de199a37f
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-09-03 07:47:01 +0300

    feat: add 03 09 2024 target

---
+++ daily-targets/2024-09-03.js
@@ -0,0 +1,13 @@
+module.exports = `
+<style>
+  * {
+    background: #F3F3F3;
+    color: #C64F4F;
+    * {
+      margin: 25px 325px 225px 25px;
+      box-shadow: 50px 0, 0 50px, 25vw 50px, 50px 25vw, 25vw 50vh, 50vh 25vw, 50vh 50vw;
+      -webkit-box-reflect: right 250px;
+    }
+  }
+</style>
+`;
