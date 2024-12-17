7a548db040b77aba078415ec3e1b20aed637c91d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-12-17 12:14:35 +0300

    test: environment

--- constatns.js
+++ constatns.js
@@ -1,5 +1,8 @@
 const process = require("node:process");
 
+console.log(process.env.HOST_NAME);
+console.log(process.env.ROOT_PATH);
+
 module.exports = {
   CSSBATTLE_HOST_NAME:
     process.env.CSSBATTLE_HOST_NAME || "https://cssbattle.dev",
