839dcdfa936f88028c82d2e807f0de7969a9772a
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-12-17 13:04:44 +0300

    chore: stable evironment

--- constatns.js
+++ constatns.js
@@ -1,8 +1,5 @@
 const process = require("node:process");
 
-console.log(process.env.HOST_NAME);
-console.log(process.env.ROOT_PATH);
-
 module.exports = {
   CSSBATTLE_HOST_NAME:
     process.env.CSSBATTLE_HOST_NAME || "https://cssbattle.dev",
