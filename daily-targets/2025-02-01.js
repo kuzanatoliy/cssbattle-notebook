efaa165f6b80e34a9c98488a423562a3cdca6f16
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-02-01 21:09:49 +0300

    feat: add 01 02 2025 target

---
+++ daily-targets/2025-02-01.js
@@ -0,0 +1,12 @@
+module.exports = `
+<style>
+  * {
+    background: #EBE77E;
+    * {
+      background: #DE817E;
+      margin: 80px 90px;
+      clip-path: polygon(0 50%, 60px 0, 100% 0, 40vw 50%, 100% 100%, 60px 100%);
+    }
+  }
+</style>
+`;
