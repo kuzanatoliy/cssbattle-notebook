7b248b999ee342da14a2ef6fef4446017c53c0b4
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-02-10 08:43:17 +0300

    feat: add 10 02 2025 target

---
+++ daily-targets/2025-02-10.js
@@ -0,0 +1,21 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #FFF;
+    body {
+      background: #328FC1;
+      margin: 75px 295px 75px 0;
+      -webkit-box-reflect: right 190px;
+      p {
+        position: fixed;
+        border-radius: 9in;
+        height: 240px;
+        width: 240px;
+        margin: -45px -175px;
+        box-shadow: 255px 0 0 -95px #328FC1;
+      }
+    }
+  }
+</style>
+`;
