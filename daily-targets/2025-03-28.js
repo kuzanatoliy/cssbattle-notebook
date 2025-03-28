cfec00558f653a2c85cbbadcd1f833c47395a64e
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-03-28 08:34:01 +0300

    feat: add 28 03 2025 target

---
+++ daily-targets/2025-03-28.js
@@ -0,0 +1,17 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: var(--b, #00B0C8);
+    body {
+      --b: #FFF;
+      margin: 80px 130px;
+      border: solid 20px #1A353C;
+      p {
+        position: fixed;
+        inset: 124px 50px;
+      }
+    }
+  }
+</style>
+`;
