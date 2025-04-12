edf06c5ba387417e563fa04466cdf4953f4f8b50
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-04-12 09:16:57 +0300

    feat: add 12 04 2025 target

---
+++ daily-targets/2025-04-12.js
@@ -0,0 +1,27 @@
+module.exports = `
+<dl><p>
+<style>
+  * {
+    background: #E3CC9E;
+    * {
+      background: #995757;
+      margin: 60px 50px;
+      dl {
+        position: fixed;
+        box-shadow: 0 180px #995757;
+      }
+      * {
+        background: #FFF;
+        padding: 10px 80px;
+        margin: -20px 0;
+        border-radius: 50%;
+        p {
+          margin: 0;
+          background: #6C3232;
+          padding: 10px 70px;
+        }
+      }
+    }
+  }
+</style>
+`;
