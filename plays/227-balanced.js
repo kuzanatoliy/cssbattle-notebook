4e308108259d53ca4774500452475318e9ed7970
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-05-09 18:42:07 +0300

    refactor: balanced play

--- plays/227-balanced.js
+++ plays/227-balanced.js
@@ -2,18 +2,17 @@ module.exports = `
 <p>
 <style>
   * {
-    background: #CF6939;
+    background: var(--b, #CF6939);
     body {
       border: solid 40px #F9D081;
       margin: 15px 130px;
       border-radius: 9in;
-      background: linear-gradient(#CF6939 80px, 0, #123747 160px, 0, #6D8971);
+      --b: linear-gradient(#CF6939 80px, 0, #6D8971);
+      padding-top: 80px;
       p {
-        position: fixed;
+        --b: #123747;
         padding: 40px 20px;
-        left: 130px;
-        top: 119px;
-        box-shadow: 25vw 0 #CF6939;
+        box-shadow: 43q 0 #CF6939, -43q 0 #CF6939;
       }
     }
   }
