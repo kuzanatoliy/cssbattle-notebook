064de68a498edaea79a8e1dcc231d573d9f119f6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-05-09 18:51:09 +0300

    refactor: another tree

--- plays/228-another-tree.js
+++ plays/228-another-tree.js
@@ -1,25 +1,23 @@
 module.exports = `
 <p><dl>
 <style>
-  *, dl {
-    background: #D2F8FF;
+  * {
+    position: fixed;
     body {
       background: #3F8090;
-      margin: 30px 140px 150px;
+      inset: 23q 140q 150q;
       border-radius: 9in;
-      p, dl {
-        position: fixed;
-      }
+      box-shadow: 0 0 0 9in #D2F8FF;
       p {
-        background: none;
         margin: 0;
-        inset: 90px 55vw -65px -50px;
+        inset: 95q 55vw -71q -50px;
         border-radius: 0 0 80%;
         box-shadow: 5vw 5vw 0 5vw #072949;
         -webkit-box-reflect: right 40px;
       }
       dl {
         inset: 254px 0 -5vw;
+        background: #D2F8FF;
       }
     }
   }
