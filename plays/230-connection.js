5f0340cd0b4077ccca35b1bdd037c2b039266999
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-05-13 08:42:30 +0300

    refactor: connection play

--- plays/230-connection.js
+++ plays/230-connection.js
@@ -1,31 +1,19 @@
 module.exports = `
-<dl><p><p>
+<p>
 <style>
-  * {
-    color: #3F4869;
+  html, body {
     background: conic-gradient(#F4DA64 225deg, #3F4869 225deg 315deg, #F4DA64 315deg);
     margin: -122px -72px;
-    border: solid var(--o, 2in);
+    border: solid var(--o, 2in) #3F4869;
     border-radius: 9in;
     * {
       background: #F4DA64;
       margin: 20px;
       --o: 5vw;
-      dl {
-        background: none;
+      p {
         position: fixed;
-        --o: none;
-        inset: 30vw 10vw;
-        p {
-          float: left;
-          padding: 10px 40px;
-          border-radius: 0;
-          --o: 0;
-          margin: 0 40px;
-          & + p {
-            padding: 10px 20px;
-          }
-        }
+        inset: 30vw 20vw;
+        background: linear-gradient(90deg, #F4DA64 50%, 0, transparent 80%, 0, #F4DA64);
       }
     }
   }
