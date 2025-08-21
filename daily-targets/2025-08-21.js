aa4339b35edf3ae2085a90a249caf1d1c2d36171
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-08-21 21:23:59 +0300

    feat: add 21 08 2025 target

---
+++ daily-targets/2025-08-21.js
@@ -0,0 +1,20 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #526766;
+    body {
+      margin: 0 225px 150px 50px;
+      box-shadow: -25px 25px 0 25px #B9C99F;
+      padding: 50px 0;
+      -webkit-box-reflect: right 50px;
+      p {
+        background: #B9C99F;
+        height: 50px;
+        border-radius: 0 9in 9in 0;
+        box-shadow: -50px 0 #526766;
+      }
+    }
+  }
+</style>
+`;
