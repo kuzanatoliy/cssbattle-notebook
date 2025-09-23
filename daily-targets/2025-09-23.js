8cdf293cb305ebb3fb4958038213e1dcf2ea7dee
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-09-23 08:39:19 +0300

    feat: add 23 09 2025 target

---
+++ daily-targets/2025-09-23.js
@@ -0,0 +1,20 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #F7FCDC;
+    border: solid 5vw #9F6766;
+    margin: 20px 60px;
+    border-bottom: none;
+    * {
+      margin: 40px 30px 20px;
+      p {
+        position: fixed;
+        inset: 40px 30px 50px;
+        background: red;
+        border: solid 75px #8AB8B6;
+      }
+    }
+  }
+</style>
+`;
