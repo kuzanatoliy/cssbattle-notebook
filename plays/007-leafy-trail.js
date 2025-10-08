4fba8f4e6f92e29f7237fd9ddf8c0622ed08995e
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-10-08 10:29:09 +0300

    refactor: update leafy trail play

--- plays/007-leafy-trail.js
+++ plays/007-leafy-trail.js
@@ -6,8 +6,8 @@ module.exports = `
       background: #F3AC3C;
       margin: 75px 75px 75px 175px;
       border-radius: 67% 0;
-      box-shadow: -50px 0 #998235, -25vw 0 #1A4341;
+      box-shadow: -53q 0 #998235, -25vw 0 #1A4341;
     }
   }
 </style>
-`
+`;
