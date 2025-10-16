45c1806c96fba25e17d690b1691d22e979ff6f0b
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-10-16 09:49:34 +0300

    refactor: update matrix play

--- plays/018-matrix.js
+++ plays/018-matrix.js
@@ -1,20 +1,19 @@
 module.exports = `
 <p><p a><p><p a><p a><p><p a><p><p><p a><p><p a>
 <style>
-  body {
+  * {
     margin: 0;
-    background: #5C434C;
-  }
-  p {
-    margin: 10px;
-    float: left;
-    width: 80px;
-    height: 80px;
-    background: #F09462;
-    border-radius: 100% 0 0;
-  }
-  [a] {
-    background: #F5D6B4;
+    background: var(--b, #5C434C);
+    p {
+      margin: 10px;
+      float: left;
+      padding: 40px;
+      --b: #F09462;
+      border-radius: 100% 0 0;
+    }
+    [a] {
+      --b: #F5D6B4;
+    }
   }
 </style>
 `;
