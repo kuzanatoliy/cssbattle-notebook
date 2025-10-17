6d6e3e3e9f599e78b5394aa9bcf92580aa2110aa
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-10-17 22:41:02 +0300

    refactor: update cups and balls play

--- plays/028-cups_&_balls.js
+++ plays/028-cups_&_balls.js
@@ -1,28 +1,25 @@
 module.exports = `
-<p a><p c b><p b><p c a><p c d><p a><p c a><p d>
+<p><p c b><p b><p c><p c b d><p><p c><p b d>
 <style>
-  body {
-    background: #1A4341;
+  * * {
+    background: var(--b, #1A4341);
     margin: 80px 60px;
-  }
-  p {
-    float: left;
-    width: 50px;
-    height: 50px;
-    background: #998235;
-    margin: 10px;
-  }
-  [c] {
-    background: #F3AC3C;
-  }
-  [a] {
-    border-radius: 50%;
-  }
-  [b] {
-    border-radius: 50% 50% 0 0;
-  }
-  [d] {
-    border-radius: 0 0 50% 50%;
+    [c] {
+      --b: #F3AC3C;
+    }
+    [b] {
+      border-radius: 50% 50% 0 0;
+    }
+    [d] {
+      scale: -1;
+    }
+    p {
+      float: left;
+      padding: 25px;
+      --b: #998235;
+      margin: 10px;
+      border-radius: 50%;
+    }
   }
 </style>
 `;
