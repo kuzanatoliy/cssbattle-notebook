17a8a65652f9e84690ce8cb9e907be77b580de35
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-10-22 15:59:10 +0300

    refactor: update leds play

--- plays/247-leds.js
+++ plays/247-leds.js
@@ -1,26 +1,22 @@
 module.exports = `
-<p a b><p><p><p><p a>
+<p a><p><p><p><p a b>
 <style>
   * {
-    background: linear-gradient(90deg, #FFF 50%, 0, #606060);
+    background: linear-gradient(90deg, #FFF 50vw, 0, #606060);
     margin: 0;
     [a] {
-      box-shadow: 85px 0 #606060, 170px 0 var(--c,), 85vh 0, 85vw 0; 
+      box-shadow: -85px 0 #FFF, -170px 0 var(--c,), -85vh 0, -85vw 0; 
     }
     [b] {
-      --c: #606060;
+      --c: #FFF;
     }
     p {
-      background: #606060;
+      color: #606060;
       height: 40px;
       width: 40px;
       border-radius: 9in;
-      margin: 10px;
-      color: #FFF;
-      box-shadow: 85vw 0;
-      & + p {
-        margin-top: 20px;
-      }
+      margin: 10px 10px 20px 350px;
+      box-shadow: -85vw 0;
     }
   }
 </style>
