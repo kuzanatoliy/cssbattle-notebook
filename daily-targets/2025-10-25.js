5e3d9b8b319eae42d8ff7a8fe627758a23f194c6
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-10-25 12:58:43 +0300

    feat: add 25 10 2025 target

---
+++ daily-targets/2025-10-25.js
@@ -0,0 +1,21 @@
+module.exports = `
+<dl><p>
+<style>
+  * {
+    background: #E8AD6D;
+    * {
+      background: #3B240C;
+      color: #3B240C;
+      margin: 62px 110px 222px 30px;
+      box-shadow: 0 80px, 0 40vw;
+      * {
+        position: fixed;
+        inset: 0 -80px 0 290px;
+        * {
+          inset: -17px -63px -17px 307px;
+        }
+      }
+    }
+  }
+</style>
+`;
