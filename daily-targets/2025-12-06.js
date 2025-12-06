1e90e339df98689e1764abdb5d06e99e62f8eaaf
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-12-06 10:58:45 +0300

    feat: add 06 12 2025 target

---
+++ daily-targets/2025-12-06.js
@@ -0,0 +1,18 @@
+module.exports = `
+<p>
+<style>
+  * {
+    background: #243D83;
+    border-radius: 40px;
+    body {
+      background: #F0CD48;
+      margin: 75px 125px;
+      padding: 10px;
+      p {
+        background: radial-gradient(circle at 50% 225px, #F0CD48 50vh, 0, #243D83 50vw, 0, #F0CD48);
+        height: 130px;
+      }
+    }
+  }
+</style>
+`;
