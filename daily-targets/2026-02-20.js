9f8969ad6b28ade4a71bdb6445c135ab3adc1d5c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-02-20 08:15:43 +0300

    feat: add 20 02 2026 target

---
+++ daily-targets/2026-02-20.js
@@ -0,0 +1,12 @@
+module.exports = `
+<style>
+  * {
+    background: #2F434E;
+    * {
+      background: #6AC09E;
+      margin: 30px 80px;
+      clip-path: polygon(40vw 0, 40vw 60px, 100% 50%, 40vw 45vw, 40vw 100%, 80px 45vw, 0 100%, 0 50%, 80px 45vw, 80px 60px, 0 50%, 0 0, 80px 60px);
+    }
+  }
+</style>
+`;
