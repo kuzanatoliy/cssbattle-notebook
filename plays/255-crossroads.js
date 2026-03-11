ab1ee21a158fb2ca479a793bd86eefdc07e473f7
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-03-11 18:33:49 +0300

    refactor: crossroads battle

--- plays/255-crossroads.js
+++ plays/255-crossroads.js
@@ -1,38 +1,28 @@
 module.exports = `
-<dl><dl r><p></dl></dl><dl s><dl r><p></dl></dl><div>
+<dl><dl><dl><dl><dl><dl><dl><dl><p>
 <style>
   * {
-    color: #F5E3B5;
     background: #469DBA;
-    [r] {
-      -webkit-box-reflect: right 5vw;
-    }
-    [s] {
-      scale: 1 -1;
-    }
-    * {
-      margin: 20px 50px;
-      div {
-        background: blue;
-        position: fixed;
-        padding: 25px;
-        margin: 0;
-        box-shadow: 90px -210px, 160px -140px;
-        color: #469DBA;
-        height: 0;
-      }
+    color: #469DBA;
+    margin: 10px 35px;
+    * * {
+      margin: 0;
+      position: fixed;
+      border: solid #F5E3B5;
+      border-width: 0 20px 20px 0;
+      padding: 50px;
       * {
-        margin: 20px;
-        height: 120px;
-        * {
-          border: solid;
-          border-width: 0 20px 20px 0;
-          height: 100px;
-          margin: 0;
-          width: 100px;
+        rotate: 90deg;
+        transform-origin: 130px 80px;
+        * * * {
+          padding: 35px;
           * {
-            width: 70px;
-            height: 70px;
+            transform-origin: 130px 95px;
+            p {
+              border: 0;
+              padding: 25px;
+              box-shadow: 70px 70px, 140px 140px;
+            }
           }
         }
       }
