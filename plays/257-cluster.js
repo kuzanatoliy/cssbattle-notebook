844efc7f2351b18633acfc3aa750597338020b6c
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-04-03 14:00:26 +0300

    refactor: cluster play

--- plays/257-cluster.js
+++ plays/257-cluster.js
@@ -1,38 +1,16 @@
 module.exports = `
-<dl><div><p><p></dl><dl>
+<p>
 <style>
-  html {
-    background: #4C455B;
-    * {
-      margin: 70px 120px;
-      dl {
-        height: 80px;
-        margin: 40px 0;
-        color: #F5E3B5;
-        box-shadow: 0 -40px, 0 40px, 40px 0;
-        & + dl {
-          height: 40px;
-          scale: -1;
-        }
-        div {
-          -webkit-box-reflect: right -80px;
-          p {
-            background: #8AB8B6;
-            position: fixed;
-            padding: 30px;
-            top: 10px;
-            left: 20px;
-            rotate: 45deg;
-            border-radius: 9in 0 9in 9in;
-            & + p {
-              top: 30px;
-              left: 40px;
-              padding: 10px;
-              background: #4C455B;
-              border-radius: 9in;          
-            }
-          }
-        }
+  * {
+    background: #FDFFE9;
+    body {
+      background: #2D3464;
+      margin: 45px 95px;
+      p {
+        color: #FDFFE9;
+        width: 40px;
+        height: 40px;
+        box-shadow: 35vh 0, 0 35vh, 170px 65px, 65px 170px, 170px 170px, 65px 65px #D95362, 35vh 35vh #D95362;
       }
     }
   }
