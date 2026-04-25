9cc46474961abaa41a421beb832cf7e399068004
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-04-25 08:14:04 +0300

    feat: add 25 04 2026 target

---
+++ daily-targets/2026-04-25.js
@@ -0,0 +1,22 @@
+module.exports = `
+<p><p a><p a><p>
+<style>
+  * * {
+    background: #405C48;
+    margin: 20px 39px;
+    p {
+      float: left;
+      padding: 130px 20px;
+      margin: 0 5px;
+      background: #F7EC7D;
+    }
+    [a] {
+      padding: 50px;
+      margin-top: 160px;
+      & + [a] {
+        margin-left: 7px;
+      }
+    }
+  }
+</style>
+`;
