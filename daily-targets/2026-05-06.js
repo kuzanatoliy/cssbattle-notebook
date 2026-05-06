0b4a793fbd9927d92a515a22375173a2054957c8
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-05-06 08:15:45 +0300

    feat: add 06 05 2025 target

---
+++ daily-targets/2026-05-06.js
@@ -0,0 +1,26 @@
+module.exports = `
+<p>
+<style>
+  html {
+    background: #4A9A86;
+    box-shadow: 25vw 25vw, -25vw -25vw;
+    margin: 100px 150px;
+    color: #F3EAD2;
+    * {
+      position: fixed;
+      padding: 35px;
+      background: conic-gradient(#4A9A86 0 90deg, #173889 90deg);
+      top: 65px;
+      left: 115px;
+      margin: 0;
+      border-radius: 9in;
+      rotate: 90deg;
+      * {
+        top: -100px;
+        left: 100px;
+        rotate: 180deg;
+      }
+    }
+  }
+</style>
+`;
